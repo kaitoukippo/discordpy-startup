0d0e94365b16e3f100914fc750c79a807fe608bb
kaitoukippo <79134988+kaitoukippo@users.noreply.github.com>
2021-02-16 18:58:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def naoki(ctx):
+    await ctx.send('is TINTIN')
 
 
 bot.run(token)
